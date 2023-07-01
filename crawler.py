8b354893b4b2f9fb73631c818e0387e4222e3430
BaileyPillon <138253619+BaileyPillon@users.noreply.github.com>
2023-07-01 10:44:44 -0400

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -20,7 +20,7 @@ def crawl():
         if page_url in pages_crawled:
             print(f'Already crawled "{page_url}"')
             continue
-        if page_url.startswith("https://www.j-archive.com/showgame.php?game_id="):
+        if page_url.startswith("https://www.j-archive.com/suggestcorrection.php?clue_id="):
             print(f'Skipping... "{page_url}"')
             continue
         if page_url.startswith("https://www.j-archive.com/showplayer.php?player_id="):
