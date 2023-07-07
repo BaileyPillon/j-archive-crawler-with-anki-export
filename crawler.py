6ba4e489dd935307a592113cebb2bd0c61d14daa
BaileyPillon <138253619+BaileyPillon@users.noreply.github.com>
2023-07-07 01:49:03 -0400

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -30,13 +30,13 @@ def crawl():
 
     if page_url in pages_crawled:
         print(f'Already crawled "{page_url}"')
-        return True
+        continue
     if page_url.startswith("https://www.j-archive.com/") and not page_url.startswith("https://www.j-archive.com/showgame.php?game_id="): #and not page_url.startswith("http://www.j-archive.com/showgame.php?game_id="):
         print(f'Skipping... "{page_url}"')
-        return True
+        continue
     if page_url.startswith("http://"):
         print(f'Encountered unsecure URL, skipping: "{page_url}"')
-        return True
+        continue
 
         try:
             response = requests.get(page_url)
