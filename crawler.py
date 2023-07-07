ab8c9614afaacc83276763a41c8fba5b0bef6193
BaileyPillon <138253619+BaileyPillon@users.noreply.github.com>
2023-07-07 17:05:23 -0400

    Update crawler.py
    fixing if statements

--- crawler.py
+++ crawler.py
@@ -40,9 +40,9 @@ def crawl():
         if page_url in pages_crawled:
             print(f'Already crawled "{page_url}"')
             continue
-        if page_url.startswith("https://www.j-archive.com/") and not page_url.startswith("https://www.j-archive.com/showgame.php?game_id="):
-            print(f'Skipping... "{page_url}"')            
-            continue
+        #if page_url.startswith("https://www.j-archive.com/") and not page_url.startswith("https://www.j-archive.com/showgame.php?game_id="):
+            #print(f'Skipping... "{page_url}"')            
+            #continue
 
         try:
             response = requests.get(page_url)
