0df18697fe3b242e17c0812ff1aefa211f26cc31
BaileyPillon <138253619+BaileyPillon@users.noreply.github.com>
2023-07-13 05:21:16 -0400

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,138 +1,145 @@
-import requests, time, logging, posixpath, concurrent.futures, urllib.request, pickle, datetime
-from bs4 import BeautifulSoup, ParserRejectedMarkup
-from urllib.parse import urlunparse, urlencode, parse_qs, quote, unquote, urlparse, urljoin
-from queue import Queue
+import requests, queue, time, logging, posixpath
+from bs4 import BeautifulSoup
 from pybloom_live import BloomFilter
+from requests.exceptions import RequestException
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import wait
+from urllib.parse import urlunparse, urlencode, parse_qs, urlparse, urljoin
 
-session = requests.Session()
-first_iter = True
+# Set up logging
+logging.basicConfig(filename='j-archive-game-ids-limited_editionv91.log', level=logging.INFO)
 
-class Crawler():
-    def __init__(self):
-        self.base_url = "https://www.j-archive.com"
-        self.desired_urls = "https://www.j-archive.com/showgame.php?game_id="
-        self.pages_to_crawl = Queue()
+class Crawler:
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.pages_to_crawl = queue.Queue()
         self.pages_to_crawl.put(self.base_url)
-        self.pages_crawled = BloomFilter(capacity=1000000, error_rate=0.01)
-        self.unsecure_url = "http://"
+        self.pages_crawled = BloomFilter(capacity=1000000, error_rate=0.1)  # Using a Bloom filter here
+        self.session = requests.Session()
+        self.unsecure_url = 'http://'
         self.request_interval = 1.0
         self.min_request_interval = 0.5
         self.max_request_interval = 5.0
-        self.max_retries = 10
-        
+        self.max_retries = 3
 
-    # Persistance        
-    def save_state(self):
-       with open("pages_to_crawl.pkl", "wb") as file:
-           pickle.dump(self.pages_to_crawl, file)
-       with open("pages_crawled.pkl", "wb") as file:
-           pickle.dump(self.pages_crawled, file)   
-       return
-   
-    def adaptive_throttle(self, response):
-        # Dict of status codes and multipliers
-        status_code_ranges = {
-            range(500, 600): 2.0,
-            range(400, 500): 1.5,
-        }
-        multiplier = next((multiplier for status_range, multiplier in status_code_ranges.items() if response.status_code in status_range), 0.9)
-        self.request_interval *= multiplier
+    def get_page_content(self, url):
+        try:
+            # Get page content
+            response = self.session.get(url, timeout=5)
+            if response.status_code == 200:
+                if 'text/html' in response.headers['Content-Type']:
+                    return BeautifulSoup(response.text, 'html.parser')
+                else:
+                    logging.info(f"Unhandled content type at {response.url}")
+            elif response.status_code == 404:
+                logging.info(f"404 error at {response.url}")
+            else:
+                logging.info(f"{response.status_code} error at {response.url}")
+        except RequestException as e:
+            logging.error(f'Failed to crawl "{url}": {str(e)}')
+        return None
 
-        if response.elapsed.total_seconds() < 0.5:
-            self.request_interval *= 0.9
-        current_hour = datetime.datetime.now().hour
-        if 9 <= current_hour < 17:
-            self.request_interval *= 1.1
-        elif 0 <= current_hour < 6:
-            self.request_interval *= 0.9
-        self.request_interval = max(self.min_request_interval, self.request_interval)
-        self.request_interval = min(self.max_request_interval, self.request_interval)
-            
     def normalize_url(self, url):
-        scheme, netloc, path, params, query, fragment = urlparse(url)
+        # Parse the url
+        parts = urlparse(url)
 
-        netloc = netloc.lower()  # Domain normalization
-        path = posixpath.normpath(unquote(path.lower()))  # Path normalization, with case norm and percent-encoding norm
+        # Normalize path by removing duplicate slashes, "." and ".."
+        path = parts.path
+        if '//' in path or '/./' in path or '/..' in path:
+            path = posixpath.normpath(path)
 
-        # Query normalization (sort keys and percent-encoding norm)
-        query_dict = parse_qs(query)
-        normalized_query = urlencode(sorted((k, sorted(v)) for k, v in query_dict.items()), doseq=True)
+        # Normalize query by sorting it
+        query = parts.query
+        if query:
+            query_dict = parse_qs(query)
+            sorted_query = sorted((k, sorted(v)) for k, v in query_dict.items())
+            query = urlencode(sorted_query, doseq=True)
 
-        fragment = ''
+        # Recreate the URL
+        normalized_url = urlunparse(
+            (parts.scheme, parts.netloc, path, parts.params, query, parts.fragment)
+        )
 
-        return urlunparse((scheme, netloc, path, params, normalized_query, fragment))
+        return normalized_url
+
+    def extract_links(self, url, soup):
+        # Extract links from the page
+        for link in soup.find_all('a', href=True):
+            link_url = link['href']
+            
+            # Converts a relative link to an absolute link
+            if not bool(urlparse(link_url).netloc):
+                link_url = urljoin(url, link_url)
+
+            if link_url not in self.pages_crawled:
+                self.pages_to_crawl.put(link_url) # Queue link to be crawled
 
     def same_domain(self, url1, url2):
+        """ Enqueue the same domain """
         return urlparse(url1).netloc == urlparse(url2).netloc
-
-    def filter_url(self, url):
-        return url.startswith(self.desired_urls)
-
+ 
+    def should_crawl(self, url):
+        url = self.normalize_url(url)
+        if "search.php" in url:
+            print(f"Skipping disallowed URL: {url}") # Respect robots.txt
+            return False
+        if url in self.pages_crawled:
+            print(f'Already crawled "{url}"')
+            return False
+        if url.startswith("https://www.j-archive.com/") and not url.startswith("https://www.j-archive.com/showgame.php?game_id="):
+            return False
+        if url.startswith(self.unsecure_url):
+            print(f'Encountered unsecure URL, skipping: "{url}"')
+            return False
+        if not self.same_domain(self.base_url, url):
+            return False
+        return True    
+ 
     def crawl(self, url):
-        first_iter = True
-        while not crawler.pages_to_crawl.empty() or first_iter == True:        
-            first_iter = False
+        if not self.should_crawl(url):
+            return
+
+        soup = self.get_page_content(url)
+        if soup is not None:
+            self.extract_links(url, soup)
+
+            if "showgame" in url:
+                print(f'Crawled desired URL: {url}')
+                logging.info(f'{url}')
+            else:
+                print(f'Crawled: {url}')
             
-            url = self.normalize_url(url)
+            # Mark the URL as crawled
+            self.pages_crawled.add(url)
             
-            if "search.php" in url:
-                print(f"Skipping disallowed URL: {url}")
-                continue
-            if url in self.pages_crawled:
-                print(f'Already crawled "{url}"')
-                continue
-            if url.startswith(self.unsecure_url):
-                print(f'Encountered unsecure URL, skipping: "{url}"')
-                continue
+        # Respect robots.txt
+        #print("Waiting 20 seconds...") # Uncomment this
+        #time.sleep(20) # Uncomment this
+
+    def start_crawling(self):
+        futures = set()
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            while True:
+                try:
+                    url = self.pages_to_crawl.get(timeout=3)
+                except queue.Empty:
+                    break
+                else:
+                    future = executor.submit(self.crawl, url)
+                    futures.add(future)
+        wait(futures)  # Wait for all threads to complete
+
+def main():
+    start_time = time.time() # Start program execution timer
+    base_url = 'https://www.j-archive.com'
+    crawler = Crawler(base_url)
+    crawler.start_crawling()
     
-            try:
-                response = session.get(url, timeout=5) 
-                self.adaptive_throttle(response)
-                if "text/html" in response.headers["content-type"]:
-                    try:
-                        soup = BeautifulSoup(response.text, 'html.parser')
-                    except ParserRejectedMarkup:
-                        logging.error(f'Failed to parse "{url}"')
-                        return
-                    #except bs4.FeatureNotFound:
-                        #logging.error(f'Failed to find a parser library for "{url}"')
-                        #return
-                if self.filter_url(url):
-                    self.pages_crawled.add(url)
-                    print(f'Successfully crawled and wrote "{url}"')
-                    logging.info(f'Crawled: {url}')
-    
-                # Enqueue same-domain URLs (ie, ensure crawler does not leave https://www.j-archive.com)
-                for link in soup.find_all('a', href=True):
-                    url = urljoin(url, link['href'])
-                    if self.same_domain(self.base_url, url) or url not in self.pages_crawled and self.filter(url): 
-                        if self.filter_url(url):
-                            print(f'Successfully crawled and wrote "{url}".')
-                            self.pages_to_crawl.put(url)
-    
-            except requests.exceptions.RequestException as e:
-                logging.error(f'Failed to crawl "{url}": {str(e)}')
-                time.sleep(3)
-            
-            finally:
-                self.save_state()
+    # Program execution time
+    execution_time = (time.time() - start_time) / 60
+    print((f"Program executed in {execution_time:.2f} minutes."))
+    execution_time = execution_time / 60
+    print((f"Program executed in {execution_time:.2f} hours."))
 
 if __name__ == "__main__":
-    crawler = Crawler()
-    logging.basicConfig(filename='j-archive_game_ids.log', level=logging.INFO)
-    logging.info('Started')
-    start_time = time.time()
-    with ThreadPoolExecutor(max_workers=10) as executor:
-       while not crawler.pages_to_crawl.empty() or first_iter == True:
-            url = crawler.pages_to_crawl.get()
-            url = crawler.normalize_url(url)
-            executor.submit(crawler.crawl, url)
-    logging.info('Finished')
-    if crawler.pages_to_crawl.empty():
-        execution_time = (time.time() - start_time) / 60
-        print((f"Program executed in {execution_time:.2f} minutes."))
-        logging.info(f"Program executed in {execution_time:.2f} minutes.")
-        execution_time = execution_time / 60
-        (f"Program executed in {execution_time:.2f} hours.")
-        logging.info(f"Program executed in {execution_time:.2f} hours.")
+    main()
